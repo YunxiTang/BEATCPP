483ad05cfd61c80057c24b929d5be51ae694d6de
YunxiTang <1155136594@link.cuhk.edu.hk>
2021-01-18 22:13:38 +0800

    operator in pointer

---
+++ A3/pointer2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	// initialize a pointer with NULL
+	int *ptr = NULL;
+	cout << "the value ptr is: " << ptr;
+	return 0;
+}
+
---
+++ A3/pointer3.cpp
@@ -0,0 +1,66 @@
+/*operators of pointer is CPP*/
+#include <iostream>
+
+using namespace std;
+const int MAX = 3;
+
+int test1(void)
+{
+	// 递增一个指针
+	int var[MAX] = {10,100,1000};
+
+	int *ptr = NULL;
+
+	ptr = var;
+	for(int i=0;i<MAX;i++)
+	{
+		cout << "Address of var[" << i << "] = " << ptr << endl;
+
+		cout << "(By Index)Value of var[" << i << "] = " << var[i] << endl;
+
+		cout << "(By Pointer)Value of var[" << i <<"] = " << *ptr << endl;
+		ptr++;
+	}
+	return 0;
+}
+
+int test2(void)
+{
+	//递减一个指针
+	int var[MAX] = {10,100,1000};
+
+	int *ptr = NULL;
+	
+	ptr = &var[MAX-1];
+
+	for(int i=MAX;i>0;i--)
+	{
+		cout << "Address of var[" << i << "] = " << ptr << endl;
+		cout << "(By Pointer) Value of var[" << i << "] = " << *ptr << endl;
+		ptr--;
+	}
+	return 0;
+}
+
+int main(void)
+{
+	int k = 1;
+
+	cin >> k;
+
+	if(k==1)
+	{
+		test1();
+	}
+	else if (k==2)
+	{
+		test2();
+	}
+	else
+	{}
+	return 0;
+}
+
+
+
+
