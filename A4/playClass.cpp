8ceaafb9c1b4139860a9987a86a08613a647ad9e
YunxiTang <1155136594@link.cuhk.edu.hk>
2021-01-20 12:17:58 +0800

    "v2"

--- A4/playClass.cpp
+++ A4/playClass.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -10,8 +11,40 @@ class Box
 		double width;
 		double height;
 		double get_Volumn(void);
+		// 构造函数 (no return. can access the private member too)
+		Box(string name); 
+
+		// 析构函数 (no return. No input. No need to call explicitly)
+		~Box();
+		// 成员函数 （定义与原型）
 		void set_box(double len, double wid, double hei);
+
+		double get_Area(void){
+			return 2 * (length * width + length * height + width * height);
+		}
+
+		void set_Name(string name){
+			Name = name;
+		}
+
+		void show_name(void){
+			cout << "Name is " << Name << endl;
+		}
+
+	private:
+		string Name;
 };
+
+Box::Box(string name)
+{
+	Name = name;
+	cout << "Creating A Box Object named " << Name << endl;
+}
+
+Box::~Box(){
+	cout << "Deleting the object named " << Name << endl; 
+}
+
 
 void Box::set_box(double len, double wid, double hei)
 {
@@ -27,11 +60,16 @@ double Box::get_Volumn(void)
 
 int main(void)
 {
-	Box mybox;
+	Box mybox("SmallBox");
 	double V;
+	double S;
 	mybox.set_box(12.0,12.3,2.0);
+	// mybox.set_Name("BigBox");
 	V = mybox.get_Volumn();
+	S = mybox.get_Area();
+	mybox.show_name();
 	cout << "Length of mybox: " << mybox.length << endl;
 	cout << "Volmun of mybox: " << V << endl;
+	cout << "Surface area: " << S << endl;
 	return 0;
 }
