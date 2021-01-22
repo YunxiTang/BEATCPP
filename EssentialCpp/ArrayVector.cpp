412e0a9f87eb3da5f369893ac37a554b923c8253
YunxiTang <1155136594@link.cuhk.edu.hk>
2021-01-22 18:34:30 +0800

    vector

--- EssentialCpp/ArrayVector.cpp
+++ EssentialCpp/ArrayVector.cpp
@@ -2,4 +2,39 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+int main(void){
+    vector<int> vec;
+    int i;
+
+    // show the original size of vec
+    cout << "size of vec: " << vec.size() << endl;
+
+    // push 5 values into vec
+    for ( i = 0; i < 5; i++)
+    {
+        vec.push_back(i);
+    }
+
+    // show the size of vec now
+    cout << "size of extended vec: " << vec.size() << endl;
+    for ( i = 0; i < 5; i++)
+    {
+        cout << "value of vec[" << i << "]: " << vec[i] << endl;
+    }
+
+    // use iterator to access value in vec
+    vector<int>::iterator v=vec.begin();
+    while (v != vec.end())
+    {
+        cout << "value of v: " << *v << endl;
+        v++;
+    }
+    
+    return 0;
+
+}
+
+
 
